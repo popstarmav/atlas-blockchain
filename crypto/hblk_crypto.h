783feb5ff1a3d29ce4ab533883237fa90f1ff6d3
popstarmav <cj.johnson2017@gmail.com>
2025-05-25 10:17:50 -0500

    Header File

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -6,6 +6,7 @@
 #include <openssl/sha.h>
 
 /* FUNCTIONS */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s,
+		size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 #endif /* HBLK_CRYPTO_H */
