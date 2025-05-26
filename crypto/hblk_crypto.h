18c5303d90a613b84655cbb8e5c52576ee615f15
popstarmav <cj.johnson2017@gmail.com>
2025-05-26 08:26:07 -0500

    Add ec_create function to generate secp256k1 key pairs

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -4,9 +4,16 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/bn.h>
+#include <openssl/obj_mac.h>
+
+/* Define the curve to use for EC operations */
+#define EC_CURVE NID_secp256k1
 
 /* FUNCTIONS */
 uint8_t *sha256(int8_t const *s,
 		size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
 
 #endif /* HBLK_CRYPTO_H */
