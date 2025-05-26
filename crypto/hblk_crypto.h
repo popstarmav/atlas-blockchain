f33006db137fc5d3622026ace434db469d6cf04a
popstarmav <cj.johnson2017@gmail.com>
2025-05-26 08:44:28 -0500

    Header File

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -10,10 +10,15 @@
 
 /* Define the curve to use for EC operations */
 #define EC_CURVE NID_secp256k1
+/* Define the curve to use for EC operations */
+#define EC_CURVE NID_secp256k1
+/* Define the length of uncompressed public key (1 + 32 + 32 = 65 bytes) */
+#define EC_PUB_LEN 65
 
 /* FUNCTIONS */
 uint8_t *sha256(int8_t const *s,
 		size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 #endif /* HBLK_CRYPTO_H */
