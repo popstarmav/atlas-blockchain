b0c8e726eb246bb74b3e79faf6b9040f0a592cf4
popstarmav <cj.johnson2017@gmail.com>
2025-05-26 08:30:32 -0500

    Add ec_create function to generate secp256k1 key pairs

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -3,25 +3,26 @@
 /**
  * ec_create - Creates a new EC key pair
  *
- * Return: Pointer to an EC_KEY structure containing both public 
+ * Return: Pointer to an EC_KEY structure containing both public
  * and privatev keys, or NULL upon failure
  */
 EC_KEY *ec_create(void)
 {
-    EC_KEY *key = NULL;
-    
-    /* Create a new EC_KEY structure */
-    key = EC_KEY_new_by_curve_name(EC_CURVE);
-    if (!key)
-        return (NULL);
-    
-    /* Generate the key pair (both private and public keys) */
-    if (EC_KEY_generate_key(key) != 1)
-    {
-        EC_KEY_free(key);
-        return (NULL);
-    }
-    
-    return (key);
+	EC_KEY *key = NULL;
+
+	/* Create a new EC_KEY structure */
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+
+	if (!key)
+		return (NULL);
+
+	/* Generate the key pair (both private and public keys) */
+	if (EC_KEY_generate_key(key) != 1)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	return (key);
 }
 
