5fcbc4c566cd44545d34eac65a55eebe7c1cb110
popstarmav <cj.johnson2017@gmail.com>
2025-05-26 23:58:58 -0500

    Add ec_to_pub function with public key extraction from EC_KEY

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -12,26 +12,26 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 	const EC_POINT *pub_key_point;
 	const EC_GROUP *group;
 	size_t pub_key_len;
-	
+
 	if (!key || !pub)
 		return (NULL);
-	
+
 	pub_key_point = EC_KEY_get0_public_key(key);
-	
+
 	if (!pub_key_point)
 		return (NULL);
-	
+
 	group = EC_KEY_get0_group(key);
-	
+
 	if (!group)
 		return (NULL);
-	
+
 	pub_key_len = EC_POINT_point2oct(group, pub_key_point,
 			POINT_CONVERSION_UNCOMPRESSED,
 			pub, EC_PUB_LEN, NULL);
-	
+
 	if (pub_key_len != EC_PUB_LEN)
 		return (NULL);
-	
+
 	return (pub);
 }
