4b891ca4549e28c9f034138d038523452f338596
popstarmav <cj.johnson2017@gmail.com>
2025-05-27 00:29:32 -0500

    Fix ec_from_pub: add missing EC_KEY_set_public_key call

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -14,18 +14,16 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
     if (pub == NULL)
         return (NULL);
-    
+
     key = EC_KEY_new();
-    
     if (key == NULL)
         return (NULL);
-    
-    group = EC_GROUP_new_by_curve_name(EC_CURVE);
 
+    group = EC_GROUP_new_by_curve_name(NID_secp256k1);
     if (group == NULL)
     {
         EC_KEY_free(key);
-            return (NULL);
+        return (NULL);
     }
 
     if (EC_KEY_set_group(key, group) != 1)
@@ -42,18 +40,28 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
         EC_KEY_free(key);
         return (NULL);
     }
-    
-    if (EC_POINT_oct2point(group, pub_point,
-            pub, EC_PUB_LEN, NULL) != 1)
+
+    if (EC_POINT_oct2point(group, pub_point, pub, EC_PUB_LEN, NULL) != 1)
     {
-                EC_POINT_free(pub_point);
-                EC_GROUP_free(group);
-                EC_KEY_free(key);
-                return (NULL);
+        EC_POINT_free(pub_point);
+        EC_GROUP_free(group);
+        EC_KEY_free(key);
+        return (NULL);
     }
 
+    /* SET the public key BEFORE freeing the point */
+    if (EC_KEY_set_public_key(key, pub_point) != 1)
+    {
+        EC_POINT_free(pub_point);
+        EC_GROUP_free(group);
+        EC_KEY_free(key);
+        return (NULL);
+    }
+
+    /* Now we can free the temporary objects */
     EC_POINT_free(pub_point);
     EC_GROUP_free(group);
-
-    return(key);
+    
+    return (key);
 }
+
