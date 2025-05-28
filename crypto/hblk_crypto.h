5535791786c91116c30a1d4b191f403e9b7da6ec
popstarmav <cj.johnson2017@gmail.com>
2025-05-28 02:52:33 -0500

    Header File

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -19,10 +19,11 @@
 /* FUNCTIONS */
 uint8_t *sha256(int8_t const *s,
 		size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
-EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+EC_KEY *ec_create(void);
+int ec_save(EC_KEY *key, char const *folder);
 
 
 #endif /* HBLK_CRYPTO_H */
