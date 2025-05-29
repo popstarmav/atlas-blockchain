6c0506ffbe2d1541ec9d3ca3dcdd524c04c06026
popstarmav <cj.johnson2017@gmail.com>
2025-05-28 21:57:01 -0500

    feat: implement ec_sign function and add sig_t structure definition

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -23,15 +23,12 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *si
 	if (!SHA256(msg, msglen, hash))
 		return (NULL);
 
-	sig->len = ECDSA_size(key);
-	if (sig->len == 0)
-		return (NULL);
+	memset(sig->sig, 0, SIG_MAX_LEN);
 
 	if (!ECDSA_sign(0, hash, SHA256_DIGEST_LENGTH, sig->sig, &sig_len, (EC_KEY *)key))
 		return (NULL);
 
-	sig->len = sig_len;
+	sig->len = (uint8_t)sig_len;
 	return (sig->sig);
 }
-
 
