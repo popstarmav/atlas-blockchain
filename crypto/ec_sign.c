b98260070e00aa37d986b3d55183edc4b6de02c4
popstarmav <cj.johnson2017@gmail.com>
2025-05-28 21:55:26 -0500

    feat: implement ec_sign function and add sig_t structure definition

---
+++ crypto/ec_sign.c
@@ -0,0 +1,37 @@
+#include "hblk_crypto.h"
+#include <openssl/sha.h>
+#include <openssl/ecdsa.h>
+#include <string.h>
+
+/**
+ * ec_sign - Signs a given set of bytes using EC_KEY private key
+ * @key: Points to the EC_KEY structure containing the private key
+ * @msg: Points to the msglen characters to be signed
+ * @msglen: Length of the message
+ * @sig: Holds the address at which to store the signature
+ *
+ * Return: Pointer to the signature buffer upon success, NULL upon failure
+ */
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+{
+	uint8_t hash[SHA256_DIGEST_LENGTH];
+	unsigned int sig_len = 0;
+
+	if (!key || !msg || !sig)
+		return (NULL);
+
+	if (!SHA256(msg, msglen, hash))
+		return (NULL);
+
+	sig->len = ECDSA_size(key);
+	if (sig->len == 0)
+		return (NULL);
+
+	if (!ECDSA_sign(0, hash, SHA256_DIGEST_LENGTH, sig->sig, &sig_len, (EC_KEY *)key))
+		return (NULL);
+
+	sig->len = sig_len;
+	return (sig->sig);
+}
+
+
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -15,10 +15,25 @@
 /* Define the length of uncompressed public key (1 + 32 + 32 = 65 bytes) */
 #define EC_PUB_LEN 65
 #define EC_PRI_LEN 32
+#define SIG_MAX_LEN 72
+
+
+/**
+ * struct sig_s - EC Signature structure
+ * @sig: Signature buffer
+ * @len: Signature size
+ */
+typedef struct sig_s
+{
+	uint8_t sig[SIG_MAX_LEN];
+	uint8_t len;
+} sig_t;
 
 /* FUNCTIONS */
 uint8_t *sha256(int8_t const *s,
 		size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *ec_sign(EC_KEY const *key,
+	uint8_t const *msg, size_t msglen, sig_t *sig);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
