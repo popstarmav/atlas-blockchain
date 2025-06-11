65c8b6c9425492cc5af4b6b7c72c06c0fcdfed4d
popstarmav <cj.johnson2017@gmail.com>
2025-06-11 05:26:48 -0500

    blockchain

--- blockchain/v0.1/test/block_create-main.c
+++ blockchain/v0.1/test/block_create-main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "blockchain.h"
 
 void _blockchain_print(blockchain_t const *blockchain);
@@ -14,42 +15,18 @@ void _blockchain_destroy(blockchain_t *blockchain);
 int main(void)
 {
     blockchain_t *blockchain;
-    block_t *genesis_block;
-    
-    printf("Creating blockchain...\n");
+    block_t *block;
+
     blockchain = blockchain_create();
-    if (!blockchain)
-    {
-        fprintf(stderr, "Failed to create blockchain\n");
-        return (EXIT_FAILURE);
-    }
-    
-    printf("Blockchain created successfully\n");
-    printf("Chain pointer: %p\n", (void*)blockchain->chain);
-    printf("Chain size: %d\n", llist_size(blockchain->chain));
-    
-    genesis_block = llist_get_head(blockchain->chain);
-    printf("Genesis block pointer: %p\n", (void*)genesis_block);
-    
-    if (genesis_block)
-    {
-        printf("Genesis block info:\n");
-        printf("  Index: %u\n", genesis_block->info.index);
-        printf("  Timestamp: %lu\n", genesis_block->info.timestamp);
-        printf("  Data length: %u\n", genesis_block->data.len);
-        printf("  Data: %.16s\n", genesis_block->data.buffer);
-    }
-    else
-    {
-        printf("ERROR: Genesis block is NULL\n");
-    }
-    
-    /* Don't call _blockchain_print for now to avoid segfault */
-    printf("Skipping _blockchain_print to avoid segfault\n");
-    
-    printf("Destroying blockchain...\n");
+    block = llist_get_head(blockchain->chain);
+
+    block = block_create(block, (int8_t *)"Holberton", 9);
+    llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+    block = block_create(block, (int8_t *)"School", 6);
+    llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+
+    _blockchain_print(blockchain);
     _blockchain_destroy(blockchain);
-    printf("Done\n");
-    
+
     return (EXIT_SUCCESS);
 }
