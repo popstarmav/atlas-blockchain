ed74b150cac0c7fca8ccb0612841997961802936
popstarmav <cj.johnson2017@gmail.com>
2025-06-11 05:02:34 -0500

    blockchain

--- blockchain/v0.1/llist.c
+++ blockchain/v0.1/llist.c
@@ -1,7 +1,7 @@
 #include "llist.h"
 #include <stdlib.h>
 
-/**
+/** 
  * llist_create - Creates a new linked list
  * @mt_support: Multi-threading support flag (ignored for now)
  *
@@ -9,18 +9,19 @@
  */
 llist_t *llist_create(int mt_support)
 {
-	llist_t *list;
-	(void)mt_support; /* Unused parameter */
-
-	list = malloc(sizeof(llist_t));
-	if (!list)
-		return (NULL);
-
-	list->node = NULL;
-	list->next = NULL;
-	list->prev = NULL;
-
-	return (list);
+    llist_t *list;
+    
+    (void)mt_support; /* Unused parameter */
+    
+    list = malloc(sizeof(llist_t));
+    if (!list)
+        return (NULL);
+        
+    list->node = NULL;
+    list->next = NULL;
+    list->prev = NULL;
+    
+    return (list);
 }
 
 /**
@@ -33,24 +34,24 @@ llist_t *llist_create(int mt_support)
  */
 int llist_destroy(llist_t *list, int free_nodes, void (*free_node)(void *))
 {
-	llist_t *current, *next;
-
-	if (!list)
-		return (-1);
-
-	current = list;
-	while (current)
-	{
-		next = current->next;
-		if (free_nodes && current->node && free_node)
-			free_node(current->node);
-		else if (free_nodes && current->node)
-			free(current->node);
-		free(current);
-		current = next;
-	}
-
-	return (0);
+    llist_t *current, *next;
+    
+    if (!list)
+        return (-1);
+        
+    current = list;
+    while (current)
+    {
+        next = current->next;
+        if (free_nodes && current->node && free_node)
+            free_node(current->node);
+        else if (free_nodes && current->node)
+            free(current->node);
+        free(current);
+        current = next;
+    }
+    
+    return (0);
 }
 
 /**
@@ -63,45 +64,51 @@ int llist_destroy(llist_t *list, int free_nodes, void (*free_node)(void *))
  */
 int llist_add_node(llist_t *list, void *node, int mode)
 {
-	llist_t *new_node, *current;
-
-	if (!list)
-		return (-1);
-
-	/* If list is empty, add as first node */
-	if (!list->node)
-	{
-		list->node = node;
-		return (0);
-	}
-
-	/* Create new node */
-	new_node = malloc(sizeof(llist_t));
-	if (!new_node)
-		return (-1);
-
-	new_node->node = node;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
-	if (mode == ADD_NODE_FRONT)
-	{
-		/* Add at front */
-		new_node->next = list;
-		list->prev = new_node;
-		/* Note: caller should update their list pointer */
-	}
-	else
-	{
-		/* Add at rear */
-		current = list;
-		while (current->next)
-			current = current->next;
-		current->next = new_node;
-		new_node->prev = current;
-	}
-
-	return (0);
+    llist_t *new_node, *current;
+    
+    if (!list)
+        return (-1);
+        
+    /* If list is empty, add as first node */
+    if (!list->node)
+    {
+        list->node = node;
+        return (0);
+    }
+    
+    /* Create new node */
+    new_node = malloc(sizeof(llist_t));
+    if (!new_node)
+        return (-1);
+        
+    new_node->node = node;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    
+    if (mode == ADD_NODE_REAR)
+    {
+        /* Add at rear */
+        current = list;
+        while (current->next)
+            current = current->next;
+        current->next = new_node;
+        new_node->prev = current;
+    }
+    else /* ADD_NODE_FRONT */
+    {
+        /* Add at front - shift existing data */
+        new_node->node = list->node;
+        new_node->next = list->next;
+        new_node->prev = list;
+        
+        if (list->next)
+            list->next->prev = new_node;
+            
+        list->node = node;
+        list->next = new_node;
+    }
+    
+    return (0);
 }
 
 /**
@@ -112,10 +119,9 @@ int llist_add_node(llist_t *list, void *node, int mode)
  */
 void *llist_get_head(llist_t *list)
 {
-	if (!list)
-		return (NULL);
-
-	return (list->node);
+    if (!list)
+        return (NULL);
+    return (list->node);
 }
 
 /**
@@ -126,16 +132,15 @@ void *llist_get_head(llist_t *list)
  */
 void *llist_get_tail(llist_t *list)
 {
-	llist_t *current;
-
-	if (!list)
-		return (NULL);
-
-	current = list;
-	while (current->next)
-		current = current->next;
-
-	return (current->node);
+    llist_t *current;
+    
+    if (!list)
+        return (NULL);
+        
+    current = list;
+    while (current->next)
+        current = current->next;
+    return (current->node);
 }
 
 /**
@@ -146,20 +151,20 @@ void *llist_get_tail(llist_t *list)
  */
 int llist_size(llist_t *list)
 {
-	llist_t *current;
-	int count = 0;
-
-	if (!list)
-		return (0);
-
-	current = list;
-	while (current && current->node)
-	{
-		count++;
-		current = current->next;
-	}
-
-	return (count);
+    llist_t *current;
+    int count = 0;
+    
+    if (!list)
+        return (0);
+        
+    current = list;
+    while (current && current->node)
+    {
+        count++;
+        current = current->next;
+    }
+    
+    return (count);
 }
 
 /**
@@ -172,20 +177,21 @@ int llist_size(llist_t *list)
  */
 int llist_for_each(llist_t *list, int (*func)(void *, unsigned int, void *), void *arg)
 {
-	llist_t *current;
-	unsigned int index = 0;
-
-	if (!list || !func)
-		return (-1);
-
-	current = list;
-	while (current && current->node)
-	{
-		if (func(current->node, index, arg) != 0)
-			return (-1);
-		current = current->next;
-		index++;
-	}
-
-	return (0);
+    llist_t *current;
+    unsigned int index = 0;
+    
+    if (!list || !func)
+        return (-1);
+        
+    current = list;
+    while (current && current->node)
+    {
+        if (func(current->node, index, arg) != 0)
+            return (-1);
+        current = current->next;
+        index++;
+    }
+    
+    return (0);
 }
+
