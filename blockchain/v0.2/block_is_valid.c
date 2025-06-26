d2f120994627a5e9e397fb192e4520394ebc675c
popstarmav <cj.johnson2017@gmail.com>
2025-06-25 23:43:25 -0500

    blockchain

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -3,56 +3,53 @@
 
 /**
  * block_is_valid - Validates block data
- * @block: Pointer to block
+ * @block: Pointer to block  
  * @prev_block: Pointer to previous block
  *
- * Return: 0 if valid, 1 if invalid (opposite of normal convention)
+ * Return: 0 if valid, 1 if invalid
  */
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
     uint8_t hash_buf[SHA256_DIGEST_LENGTH];
 
     if (!block)
-        return (1); /* Invalid */
+        return (1);
 
-    /* Handle genesis block */
+    /* Genesis block case */
     if (block->info.index == 0)
     {
-        if (!prev_block)
-            return (memcmp(block, &_genesis, sizeof(_genesis)) != 0 ? 1 : 0);
-        else
-            return (1); /* Genesis should not have prev_block */
+        return (memcmp(block, &_genesis, sizeof(_genesis)) == 0 ? 0 : 1);
     }
 
-    /* Non-genesis block must have prev_block */
+    /* Non-genesis blocks must have prev_block */
     if (!prev_block)
-        return (1); /* Invalid */
+        return (1);
 
     /* Check index sequence */
     if (block->info.index != prev_block->info.index + 1)
-        return (1); /* Invalid */
+        return (1);
 
-    /* Verify previous block's hash matches what we store */
+    /* Compute previous block's hash and verify it matches stored hash */
     if (!block_hash(prev_block, hash_buf))
-        return (1); /* Invalid */
+        return (1);
 
     if (memcmp(hash_buf, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
-        return (1); /* Invalid */
+        return (1);
 
-    /* Check if prev_hash in current block matches previous block's hash */
+    /* Check prev_hash in current block matches previous block's hash */
     if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH) != 0)
-        return (1); /* Invalid */
+        return (1);
 
-    /* Verify current block's hash */
+    /* Verify current block's computed hash matches stored hash */
     if (!block_hash(block, hash_buf))
-        return (1); /* Invalid */
+        return (1);
 
     if (memcmp(hash_buf, block->hash, SHA256_DIGEST_LENGTH) != 0)
-        return (1); /* Invalid */
+        return (1);
 
     /* Check if hash matches difficulty */
     if (!hash_matches_difficulty(block->hash, block->info.difficulty))
-        return (1); /* Invalid */
+        return (1);
 
     return (0); /* Valid */
 }
--- blockchain/v0.2/provided/_blockchain_print.c
+++ blockchain/v0.2/provided/_blockchain_print.c
@@ -1,6 +1,6 @@
 #include "../blockchain.h"
 
-/**
+/** 
  * _print_hex_buffer - Prints a buffer in hexadecimal format
  * @buf: Pointer to the buffer to be printed
  * @len: Number of bytes from @buf to be printed
@@ -8,51 +8,15 @@
 static void _print_hex_buffer(uint8_t const *buf, size_t len)
 {
     size_t i;
-
+    
     for (i = 0; buf && i < len; i++)
         printf("%02x", buf[i]);
 }
 
 /**
- * _block_print - Prints information about a Block
- * @block: Pointer to the Block to be printed
- * @index: Index of the Block in the Blockchain
- * @indent: String to be printed before each line
- *
- * Return: 0
- */
-static int _block_print(block_t const *block, unsigned int index, char const *indent)
-{
-    if (!block)
-    {
-        printf("%sBlock->NULL\n", indent);
-        return (1);
-    }
-
-    printf("%sBlock: {\n", indent);
-    printf("%s\tinfo: {\n", indent);
-    printf("%s\t\tindex: %u,\n", indent, block->info.index);
-    printf("%s\t\tdifficulty: %u,\n", indent, block->info.difficulty);
-    printf("%s\t\ttimestamp: %lu,\n", indent, block->info.timestamp);
-    printf("%s\t\tnonce: %lu,\n", indent, block->info.nonce);
-    printf("%s\t\tprev_hash: ", indent);
-    _print_hex_buffer(block->info.prev_hash, SHA256_DIGEST_LENGTH);
-    printf("\n%s\t},\n", indent);
-    printf("%s\tdata: {\n", indent);
-    printf("%s\t\tbuffer: \"%s\",\n", indent, (char const *)block->data.buffer);
-    printf("%s\t\tlen: %u\n", indent, block->data.len);
-    printf("%s\t},\n", indent);
-    printf("%s\thash: ", indent);
-    _print_hex_buffer(block->hash, SHA256_DIGEST_LENGTH);
-    printf("\n%s} -> %u\n", indent, index);
-
-    return (0);
-}
-
-/**
  * _block_print_brief - Prints information about a Block (brief)
  * @block: Pointer to the Block to be printed
- * @index: Index of the Block in the Blockchain
+ * @index: Index of the Block in the Blockchain  
  * @indent: String to be printed before each line
  *
  * Return: 0
@@ -64,33 +28,21 @@ static int _block_print_brief(block_t const *block, unsigned int index, char con
         printf("%sBlock->NULL\n", indent);
         return (1);
     }
-
-    printf("%sBlock: {", indent);
-    printf("%u, %u, %lu, %lu, ", block->info.index, block->info.difficulty,
+    
+    printf("%sBlock: {\n", indent);
+    printf("%s\tinfo: { %u, %u, %lu, %lu, ", indent, 
+           block->info.index, block->info.difficulty,
            block->info.timestamp, block->info.nonce);
     _print_hex_buffer(block->info.prev_hash, SHA256_DIGEST_LENGTH);
-    printf(", ");
-    printf("\"%s\", ", (char const *)block->data.buffer);
-    printf("%u, ", block->data.len);
+    printf(" },\n");
+    printf("%s\tdata: { \"%s\", %u },\n", indent, 
+           (char const *)block->data.buffer, block->data.len);
+    printf("%s\thash: ", indent);
     _print_hex_buffer(block->hash, SHA256_DIGEST_LENGTH);
-    printf("} -> %u\n", index);
-
+    printf("\n%s}\n", indent);
+    
     (void)index;
     return (0);
-}
-
-/**
- * _blockchain_print - Prints an entire Blockchain
- * @blockchain: Pointer to the Blockchain to be printed
- */
-void _blockchain_print(blockchain_t const *blockchain)
-{
-    printf("Blockchain: {\n");
-    printf("\tchain [%d]: [\n", llist_size(blockchain->chain));
-    llist_for_each(blockchain->chain, (node_func_t)_block_print, "\t\t");
-    printf("\t]\n");
-    printf("}\n");
-    fflush(NULL);
 }
 
 /**
