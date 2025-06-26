508ca4443637715aa31fcb6dc5eb85da81ef7671
popstarmav <cj.johnson2017@gmail.com>
2025-06-25 23:59:32 -0500

    blockchain

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,27 +1,33 @@
 #include "blockchain.h"
 
 /**
- * block_mine - Mines a Block in order to insert it in the Blockchain
+ * block_mine - Mines a block by finding a nonce that satisfies the difficulty
  * @block: Pointer to the Block to be mined
+ *
+ * Description: This function increments the nonce until the block's hash
+ * matches the required difficulty (number of leading zero bits).
  */
 void block_mine(block_t *block)
 {
-    uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-    
     if (!block)
         return;
-    
-    for (block->info.nonce = 0; block->info.nonce != UINT64_MAX; block->info.nonce++)
+
+    /* Start with nonce = 0 and increment until we find a valid hash */
+    block->info.nonce = 0;
+
+    /* Keep trying different nonce values until hash matches difficulty */
+    while (1)
     {
-        if (!block_hash(block, hash_buf))
+        /* Compute hash with current nonce */
+        if (!block_hash(block, block->hash))
             return;
-            
-        if (hash_matches_difficulty(hash_buf, block->info.difficulty))
-        {
-            /* Copy the valid hash to the block */
-            memcpy(block->hash, hash_buf, SHA256_DIGEST_LENGTH);
-            return;
-        }
+
+        /* Check if hash matches the required difficulty */
+        if (hash_matches_difficulty(block->hash, block->info.difficulty))
+            break;
+
+        /* Increment nonce and try again */
+        block->info.nonce++;
     }
 }
 
