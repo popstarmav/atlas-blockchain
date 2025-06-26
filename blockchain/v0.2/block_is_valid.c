94a1c7da9ceeff8cf8639dbf28d6b77a6755c5da
popstarmav <cj.johnson2017@gmail.com>
2025-06-25 23:22:48 -0500

    blockain

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -2,49 +2,46 @@
 
 /**
  * block_is_valid - Verifies that a Block is valid
- * @block: Block to verify
- * @prev_block: Previous Block in the Blockchain
+ * @block: Pointer to the Block to check
+ * @prev_block: Pointer to the previous Block in the Blockchain
  *
  * Return: 1 if valid, 0 otherwise
  */
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
     uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-    uint32_t i;
 
     if (!block)
         return (0);
 
-    /* Genesis Block validation */
+    /* Genesis block validation */
     if (!prev_block)
     {
+        /* Should be the genesis block */
         if (memcmp(block, &_genesis, sizeof(_genesis)) != 0)
             return (0);
         return (1);
     }
 
-    /* Check if previous hash matches */
-    if (memcmp(block->info.prev_hash, prev_block->hash,
-               SHA256_DIGEST_LENGTH) != 0)
-        return (0);
-
-    /* Check if block index is correct */
+    /* Check if index is correct */
     if (block->info.index != prev_block->info.index + 1)
         return (0);
 
-    /* Verify block hash */
-    if (!block_hash(block, hash_buf) ||
-        memcmp(block->hash, hash_buf, SHA256_DIGEST_LENGTH) != 0)
+    /* Check if prev_hash matches previous block's hash */
+    if (memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
         return (0);
 
-    /* Check if hash matches difficulty (leading zeros) */
-    for (i = 0; i < block->info.difficulty; i++)
-    {
-        if (block->hash[i] != 0)
-            return (0);
-    }
+    /* Verify the block's hash */
+    if (!block_hash(block, hash_buf))
+        return (0);
+
+    if (memcmp(block->hash, hash_buf, SHA256_DIGEST_LENGTH) != 0)
+        return (0);
+
+    /* Check if hash matches difficulty */
+    if (!hash_matches_difficulty(block->hash, block->info.difficulty))
+        return (0);
 
     return (1);
 }
-
 
--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -3,38 +3,25 @@
 /**
  * block_mine - Mines a Block in order to insert it in the Blockchain
  * @block: Pointer to the Block to be mined
- *
- * Description: This function finds a hash for block that matches its difficulty
- * by incrementing the nonce until the hash has the required number of leading
- * zero bits.
  */
 void block_mine(block_t *block)
 {
     uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-
+    
     if (!block)
         return;
-
-    /* Keep incrementing nonce until we find a hash that matches difficulty */
-    while (1)
+    
+    for (block->info.nonce = 0; block->info.nonce != UINT64_MAX; block->info.nonce++)
     {
-        /* Update timestamp for each attempt */
-        block->info.timestamp = time(NULL);
-        
-        /* Compute hash with current nonce */
         if (!block_hash(block, hash_buf))
             return;
-
-        /* Check if hash matches difficulty */
+            
         if (hash_matches_difficulty(hash_buf, block->info.difficulty))
         {
-            /* Found a valid hash, copy it to block */
+            /* Copy the valid hash to the block */
             memcpy(block->hash, hash_buf, SHA256_DIGEST_LENGTH);
-            break;
+            return;
         }
-
-        /* Increment nonce and try again */
-        block->info.nonce++;
     }
 }
 
