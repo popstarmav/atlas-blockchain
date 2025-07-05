55a8f68641b609a100375dbc9997094d74d8ffd5
popstarmav <cj.johnson2017@gmail.com>
2025-07-05 14:25:37 -0500

    blockchain v3

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -14,7 +14,7 @@
  * output upon success, or NULL upon failure
  */
 unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash
-		[SHA256_DIGEST_LENGTH],uint8_t tx_id[SHA256_DIGEST_LENGTH],
+		[SHA256_DIGEST_LENGTH], uint8_t tx_id[SHA256_DIGEST_LENGTH],
                                        tx_out_t const *out)
 
 {
