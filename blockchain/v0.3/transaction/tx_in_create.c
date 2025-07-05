514be9bbf6c5480eb0e2e48008006eaa2c70137a
popstarmav <cj.johnson2017@gmail.com>
2025-07-05 17:04:51 -0500

    blockchain v3

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -5,28 +5,29 @@
  * tx_in_create - Allocates and initializes a transaction input structure
  * @unspent: Pointer to the unspent transaction output to be converted
  *
- * Return: Pointer to the created transaction input upon success, or NULL upon failure
+ * Return: Pointer to the created transaction 
+ * input upon success, or NULL upon failure
  */
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 {
-    tx_in_t *tx_in;
+	tx_in_t *tx_in;
 
-    if (!unspent)
-        return (NULL);
+	if (!unspent)
+		return (NULL);
 
-    tx_in = calloc(1, sizeof(tx_in_t));
-    if (!tx_in)
-        return (NULL);
+	tx_in = calloc(1, sizeof(tx_in_t));
 
-    /* Copy block hash from unspent transaction output */
-    memcpy(tx_in->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
+	if (!tx_in)
+		return (NULL);
 
-    /* Copy transaction ID from unspent transaction output */
-    memcpy(tx_in->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
+	/* Copy block hash from unspent transaction output */
+	memcpy(tx_in->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
 
-    /* Copy transaction output hash from the embedded tx_out_t structure */
-    memcpy(tx_in->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
+	/* Copy transaction ID from unspent transaction output */
+	memcpy(tx_in->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
 
-    return (tx_in);
+	/* Copy transaction output hash from the embedded tx_out_t structure */
+	memcpy(tx_in->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
+
+	return (tx_in);
 }
-
