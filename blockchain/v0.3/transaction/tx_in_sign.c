6d70660282f0dd62e64a6f022fa1bdb28186ea79
popstarmav <cj.johnson2017@gmail.com>
2025-07-06 16:09:56 -0500

    blockchain v3

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -2,36 +2,6 @@
 #include "blockchain.h"
 
 /**
- * find_matching_utxo - Searches for unspent output matching the input
- * @utxo_list: List of unspent transaction outputs
- * @target_hash: Hash of the output we're looking for
- *
- * Return: Pointer to matching unspent output, or NULL if not found
- */
-static unspent_tx_out_t *find_matching_utxo(llist_t *utxo_list,
-                                             uint8_t *target_hash)
-{
-	unspent_tx_out_t *current_utxo;
-	int list_length, index;
-
-	if (!utxo_list || !target_hash)
-		return (NULL);
-
-	list_length = llist_size(utxo_list);
-	for (index = 0; index < list_length; index++)
-	{
-		current_utxo = (unspent_tx_out_t *)llist_get_node_at(utxo_list, index);
-		if (!current_utxo)
-			continue;
-
-		if (memcmp(current_utxo->out.hash, target_hash,
-			   SHA256_DIGEST_LENGTH) == 0)
-			return (current_utxo);
-	}
-	return (NULL);
-}
-
-/**
  * tx_in_sign - Signs a transaction input
  * @in: Points to the transaction input structure to sign
  * @tx_id: Contains the ID (hash) of the transaction
@@ -43,30 +13,48 @@ static unspent_tx_out_t *find_matching_utxo(llist_t *utxo_list,
 sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 		  EC_KEY const *sender, llist_t *all_unspent)
 {
-	unspent_tx_out_t *referenced_output;
+	unspent_tx_out_t *utxo;
 	uint8_t sender_pub[EC_PUB_LEN];
+	int i, list_size;
 
 	/* Validate all required parameters */
 	if (!in || !tx_id || !sender || !all_unspent)
-		return (NULL);
-
-	/* Locate the unspent output referenced by this input */
-	referenced_output = find_matching_utxo(all_unspent, in->tx_out_hash);
-	if (!referenced_output)
 		return (NULL);
 
 	/* Extract public key from sender's private key */
 	if (!ec_to_pub(sender, sender_pub))
 		return (NULL);
 
-	/* Verify sender owns the referenced output by comparing public keys */
-	if (memcmp(sender_pub, referenced_output->out.pub, EC_PUB_LEN) != 0)
+	/* Find the matching UTXO */
+	list_size = llist_size(all_unspent);
+	utxo = NULL;
+	
+	for (i = 0; i < list_size; i++)
+	{
+		utxo = (unspent_tx_out_t *)llist_get_node_at(all_unspent, i);
+		if (!utxo)
+			continue;
+			
+		if (memcmp(utxo->out.hash, in->tx_out_hash, SHA256_DIGEST_LENGTH) == 0)
+			break;
+		utxo = NULL;
+	}
+
+	/* UTXO not found */
+	if (!utxo)
 		return (NULL);
 
-	/* Initialize signature structure to zero */
+	/* 
+	 * OWNERSHIP CHECK: Compare sender's public key with UTXO's public key
+	 * If they don't match, the sender doesn't own this UTXO
+	 */
+	if (memcmp(sender_pub, utxo->out.pub, EC_PUB_LEN) != 0)
+		return (NULL);
+
+	/* Initialize signature */
 	memset(&in->sig, 0, sizeof(sig_t));
 
-	/* Create digital signature for the transaction using ec_sign */
+	/* Sign the transaction */
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
 
