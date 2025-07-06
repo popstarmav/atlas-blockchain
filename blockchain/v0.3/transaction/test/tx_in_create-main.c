d66ffe08c575fbea69c228b4894b3ac593a2a178
popstarmav <cj.johnson2017@gmail.com>
2025-07-06 16:05:49 -0500

    blockchain v3

--- blockchain/v0.3/transaction/test/tx_in_create-main.c
+++ blockchain/v0.3/transaction/test/tx_in_create-main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "blockchain.h"
 
 void _transaction_print(transaction_t const *transaction);
@@ -24,24 +25,20 @@ int main(void)
 
     /* Create a mock Block hash */
     sha256((int8_t *)"Block", strlen("Block"), block_hash);
-
     /*
      * Create a mock Transaction hash
      * This hash represents the transaction our mock output is from, and
      * our `owner` will receive the coins from
      */
     sha256((int8_t *)"Transaction", strlen("Transaction"), transaction_id);
-
     /*
      * In this example, `owner` will be the receiver of the transaction
      * output, and the sender of the transaction input we want to sign
      */
     owner = ec_create();
-
     /* Create a mock transaction output to give coins to our `owner` */
     out = tx_out_create(972, ec_to_pub(owner, pub));
     unspent = unspent_tx_out_create(block_hash, transaction_id, out);
-
     /*
      * The list of all unspent transaction outputs is normally located
      * in our Blockchain
@@ -51,34 +48,27 @@ int main(void)
 
     transaction.inputs = llist_create(MT_SUPPORT_FALSE);
     transaction.outputs = llist_create(MT_SUPPORT_FALSE);
-
     /* Create the transaction input from the unspent transaction output */
     in = tx_in_create(unspent);
     llist_add_node(transaction.inputs, in, ADD_NODE_REAR);
-
     pub[1] *= 2; /* Pretend it's a different address :) */
     out2 = tx_out_create(972, pub);
     llist_add_node(transaction.outputs, out2, ADD_NODE_REAR);
 
     /* Compute transaction id (hash) */
     transaction_hash(&transaction, transaction.id);
-
     /* Sign transaction input with transaction hash */
     tx_in_sign(in, transaction.id, owner, all_unspent);
-
     _transaction_print(&transaction);
-
     if (ec_verify(owner, transaction.id, sizeof(transaction.id), &in->sig))
         printf("Signature verified\n");
     else
         fprintf(stderr, "Signature invalid\n");
 
-    /* Cleanup - Use ec_free instead of deprecated EC_KEY_free */
-    ec_free(owner);
+    /* Cleanup */
+    EC_KEY_free(owner);
     free(out);
     llist_destroy(all_unspent, 1, free);
     free(in);
-
     return (EXIT_SUCCESS);
 }
-
