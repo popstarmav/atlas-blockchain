2232f15e09a6e26487408a187e47a4a413df66f9
popstarmav <cj.johnson2017@gmail.com>
2025-07-06 12:57:13 -0500

    blockchain v3

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -8,8 +8,8 @@
  *
  * Return: Pointer to matching unspent output, or NULL if not found
  */
-static unspent_tx_out_t *find_matching_utxo(llist_t *utxo_list, 
-					     uint8_t *target_hash)
+static unspent_tx_out_t *find_matching_utxo(llist_t *utxo_list,
+                                             uint8_t *target_hash)
 {
 	unspent_tx_out_t *current_utxo;
 	int list_length, index;
@@ -18,18 +18,16 @@ static unspent_tx_out_t *find_matching_utxo(llist_t *utxo_list,
 		return (NULL);
 
 	list_length = llist_size(utxo_list);
-	
 	for (index = 0; index < list_length; index++)
 	{
 		current_utxo = (unspent_tx_out_t *)llist_get_node_at(utxo_list, index);
 		if (!current_utxo)
 			continue;
-			
-		if (memcmp(current_utxo->out.hash, target_hash, 
-			   (size_t)SHA256_DIGEST_LENGTH) == 0)
+
+		if (memcmp(current_utxo->out.hash, target_hash,
+			   SHA256_DIGEST_LENGTH) == 0)
 			return (current_utxo);
 	}
-	
 	return (NULL);
 }
 
@@ -47,10 +45,15 @@ static int validate_ownership(EC_KEY const *private_key, tx_out_t *output)
 	if (!private_key || !output)
 		return (0);
 
+	/* Extract public key from private key */
 	if (!ec_to_pub(private_key, public_key_buffer))
 		return (0);
 
-	return (memcmp(public_key_buffer, output->pub, (size_t)EC_PUB_LEN) == 0);
+	/* Compare with the public key in the output */
+	if (memcmp(public_key_buffer, output->pub, EC_PUB_LEN) != 0)
+		return (0);
+
+	return (1);
 }
 
 /**
@@ -79,6 +82,9 @@ sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 	/* Verify sender has ownership of the referenced output */
 	if (!validate_ownership(sender, &referenced_output->out))
 		return (NULL);
+
+	/* Initialize signature structure */
+	memset(&in->sig, 0, sizeof(sig_t));
 
 	/* Create digital signature for the transaction using ec_sign */
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
