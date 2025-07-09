ff8b82a5035ad983c39bec8f402c3c60d115d4d2
popstarmav <cj.johnson2017@gmail.com>
2025-07-09 18:51:15 -0500

    blockchain v3

--- blockchain/v0.3/transaction/coinbase_create.c
+++ blockchain/v0.3/transaction/coinbase_create.c
@@ -3,6 +3,70 @@
 #include "transaction.h"
 
 /**
+ * create_transaction_lists - Creates input and output lists for a transaction
+ * @transaction: Transaction to initialize
+ *
+ * Return: 1 on success, 0 on failure
+ */
+static int create_transaction_lists(transaction_t *transaction)
+{
+	transaction->inputs = llist_create(MT_SUPPORT_FALSE);
+	if (!transaction->inputs)
+		return (0);
+
+	transaction->outputs = llist_create(MT_SUPPORT_FALSE);
+	if (!transaction->outputs)
+	{
+		llist_destroy(transaction->inputs, 1, NULL);
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
+ * create_coinbase_input - Creates a coinbase transaction input
+ * @block_index: Index of the Block the coinbase transaction will belong to
+ *
+ * Return: Pointer to the created input, or NULL on failure
+ */
+static tx_in_t *create_coinbase_input(uint32_t block_index)
+{
+	tx_in_t *tx_in;
+
+	tx_in = calloc(1, sizeof(tx_in_t));
+	if (!tx_in)
+		return (NULL);
+
+	/* Set block_index in the first 4 bytes of tx_out_hash to make it unique */
+	memcpy(tx_in->tx_out_hash, &block_index, sizeof(block_index));
+
+	return (tx_in);
+}
+
+/**
+ * cleanup_transaction - Frees all resources of a transaction
+ * @transaction: Transaction to clean up
+ * @tx_in: Transaction input to free if not NULL
+ * @tx_out: Transaction output to free if not NULL
+ */
+static void cleanup_transaction(
+	transaction_t *transaction,
+	tx_in_t *tx_in,
+	tx_out_t *tx_out)
+{
+	if (tx_in)
+		free(tx_in);
+	if (tx_out)
+		free(tx_out);
+	if (transaction->inputs)
+		llist_destroy(transaction->inputs, 1, free);
+	if (transaction->outputs)
+		llist_destroy(transaction->outputs, 1, free);
+	free(transaction);
+}
+
+/**
  * coinbase_create - Creates a coinbase transaction
  * @receiver: Contains the public key of the miner who will receive the coins
  * @block_index: Index of the Block the coinbase transaction will belong to
@@ -12,88 +76,40 @@
 transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index)
 {
 	transaction_t *transaction;
-	tx_in_t *tx_in;
-	tx_out_t *tx_out;
+	tx_in_t *tx_in = NULL;
+	tx_out_t *tx_out = NULL;
 	uint8_t receiver_pub[EC_PUB_LEN];
 
-	if (!receiver)
+	if (!receiver || !ec_to_pub(receiver, receiver_pub))
 		return (NULL);
 
-	/* Extract receiver's public key */
-	if (!ec_to_pub(receiver, receiver_pub))
-		return (NULL);
-
-	/* Create transaction structure */
 	transaction = calloc(1, sizeof(transaction_t));
 	if (!transaction)
 		return (NULL);
 
-	/* Create inputs list */
-	transaction->inputs = llist_create(MT_SUPPORT_FALSE);
-	if (!transaction->inputs)
+	if (!create_transaction_lists(transaction))
 	{
 		free(transaction);
 		return (NULL);
 	}
 
-	/* Create outputs list */
-	transaction->outputs = llist_create(MT_SUPPORT_FALSE);
-	if (!transaction->outputs)
+	tx_in = create_coinbase_input(block_index);
+	if (!tx_in || llist_add_node(transaction->inputs, tx_in, ADD_NODE_REAR) != 0)
 	{
-		llist_destroy(transaction->inputs, 1, NULL);
-		free(transaction);
+		cleanup_transaction(transaction, tx_in, NULL);
 		return (NULL);
 	}
 
-	/* Create transaction input (zeroed) */
-	tx_in = calloc(1, sizeof(tx_in_t));
-	if (!tx_in)
-	{
-		llist_destroy(transaction->inputs, 1, NULL);
-		llist_destroy(transaction->outputs, 1, NULL);
-		free(transaction);
-		return (NULL);
-	}
-
-	/* Set block_index in the first 4 bytes of tx_out_hash to make it unique */
-	memcpy(tx_in->tx_out_hash, &block_index, sizeof(block_index));
-
-	/* Add input to transaction */
-	if (llist_add_node(transaction->inputs, tx_in, ADD_NODE_REAR) != 0)
-	{
-		free(tx_in);
-		llist_destroy(transaction->inputs, 1, NULL);
-		llist_destroy(transaction->outputs, 1, NULL);
-		free(transaction);
-		return (NULL);
-	}
-
-	/* Create transaction output */
 	tx_out = tx_out_create(COINBASE_AMOUNT, receiver_pub);
-	if (!tx_out)
+	if (!tx_out || llist_add_node(transaction->outputs, tx_out, ADD_NODE_REAR) != 0)
 	{
-		llist_destroy(transaction->inputs, 1, free);
-		llist_destroy(transaction->outputs, 1, NULL);
-		free(transaction);
+		cleanup_transaction(transaction, NULL, tx_out);
 		return (NULL);
 	}
 
-	/* Add output to transaction */
-	if (llist_add_node(transaction->outputs, tx_out, ADD_NODE_REAR) != 0)
-	{
-		free(tx_out);
-		llist_destroy(transaction->inputs, 1, free);
-		llist_destroy(transaction->outputs, 1, NULL);
-		free(transaction);
-		return (NULL);
-	}
-
-	/* Compute transaction hash (ID) */
 	if (!transaction_hash(transaction, transaction->id))
 	{
-		llist_destroy(transaction->inputs, 1, free);
-		llist_destroy(transaction->outputs, 1, free);
-		free(transaction);
+		cleanup_transaction(transaction, NULL, NULL);
 		return (NULL);
 	}
 
